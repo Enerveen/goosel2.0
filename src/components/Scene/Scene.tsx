6f87583fbdcadae8759c9ad7553a342213be2df7
IMConstant <kostjakov@gmail.com>
2023-01-27 20:30:42 +0300

    add cam

--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -3,11 +3,19 @@ import {observer} from "mobx-react-lite";
 import {SimulationStore} from "../../stores/simulationStore";
 import {getRandomInRange} from "../../utils/utils";
 import Plant from "../../entities/Plant";
-import {generateAnimals, generateFood, getRandomPosition, handleCanvasClick} from "../../utils/helpers";
+import {
+    generateAnimals,
+    generateFood,
+    getRandomPosition,
+    handleCanvasClick, handleCanvasMouseMove,
+    handleCanvasMousePress, handleCanvasMouseRelease, handleCanvasMouseWheel
+} from "../../utils/helpers";
 import View from "../../utils/View";
 import {appPhase} from "../../types";
 import useWindowSize from "../../hooks/useWindowSize";
 import {appConstants} from "../../constants/simulation";
+
+import {Camera} from "../../core/Camera";
 
 interface ISceneProps {
     store: SimulationStore,
@@ -24,6 +32,7 @@ const Scene = observer(({store, setAppPhase}: ISceneProps) => {
     }), [canvasWidth, canvasHeight])
 
     const view = useMemo(() => new View(context), [context])
+    const mainCamera = new Camera({ x: canvasWidth / 2, y: canvasHeight / 2 }, {x: canvasWidth, y: canvasHeight})
 
     const step = useCallback(() => {
         const timestamp = store.getTimestamp
@@ -38,57 +47,66 @@ const Scene = observer(({store, setAppPhase}: ISceneProps) => {
         } else {
             store.clearAnimalCorpses()
             store.gatherStatistics()
-            if (context) {
-                context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-                context.textAlign = 'center';
-                context.font = "bold 18px Comic Sans MS"
-                view.drawBackground({width:canvasWidth, height:canvasHeight})
-                if (!getRandomInRange(0, store.getSimulationConstants.foodSpawnChanceK / store.simulationSpeed)) {
-                    store.addPlant(new Plant({
-                        id: `P${store.getId}`,
-                        nutritionValue: getRandomInRange(
-                            store.getSimulationConstants.foodNutritionMin,
-                            store.getSimulationConstants.foodNutritionMax
-                        ),
-                        position: getRandomPosition(fieldSize.edgeX, fieldSize.edgeY)
-                    }))
-                }
-                store.getPlants.forEach(entity => {
-                    view.drawPlant(entity.position)
-                })
-                store.getAnimals.forEach(entity => {
-                    entity.live(
-                        timestamp,
-                        store.getPlants,
-                        store.getAnimals,
-                        store.removePlant,
-                        store.addAnimal,
-                        {
-                            width: fieldSize.edgeX,
-                            height: fieldSize.edgeY
-                        },
-                        store.simulationSpeed,
-                        store.getSimulationConstants.breedingMinAge,
-                        store.getSimulationConstants.breedingMaxAge,
-                        store.getSimulationConstants.breedingMaxProgress
-                    )
-                    view.drawAnimal(entity.position,
-                        timestamp - entity.age.birthTimestamp,
-                        {
-                            gender: entity.gender,
-                            age: entity.age.current,
-                            isAlive: entity.isAlive,
-                            name: entity.name
-                        }
-                    )
-                    if (entity.currentActivity.activity === 'breeding') {
-                        view.drawBreeding({x: entity.position.x + 30, y: entity.position.y - 60})
-                    }
-                    if (entity.id === store.getActiveEntity?.id) {
-                        store.setActiveEntity(entity)
-                    }
-                })
+
+            if (!context) {
+                return;
             }
+
+            context.resetTransform();
+            context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+            const aspect = { x: canvasWidth / mainCamera.fov.x / mainCamera.scale.x, y: canvasHeight / mainCamera.fov.y / mainCamera.scale.y };
+            context.translate(canvasWidth / 2 - mainCamera.position.x * aspect.x, canvasHeight / 2 - mainCamera.position.y * aspect.y);
+            context.scale(aspect.x, aspect.y);
+
+            context.textAlign = 'center';
+            context.font = "bold 18px Comic Sans MS"
+            view.drawBackground({width:canvasWidth, height:canvasHeight})
+            if (!getRandomInRange(0, store.getSimulationConstants.foodSpawnChanceK / store.simulationSpeed)) {
+                store.addPlant(new Plant({
+                    id: `P${store.getId}`,
+                    nutritionValue: getRandomInRange(
+                        store.getSimulationConstants.foodNutritionMin,
+                        store.getSimulationConstants.foodNutritionMax
+                    ),
+                    position: getRandomPosition(fieldSize.edgeX, fieldSize.edgeY)
+                }))
+            }
+            store.getPlants.forEach(entity => {
+                view.drawPlant(entity.position)
+            })
+            store.getAnimals.forEach(entity => {
+                entity.live(
+                    timestamp,
+                    store.getPlants,
+                    store.getAnimals,
+                    store.removePlant,
+                    store.addAnimal,
+                    {
+                        width: fieldSize.edgeX,
+                        height: fieldSize.edgeY
+                    },
+                    store.simulationSpeed,
+                    store.getSimulationConstants.breedingMinAge,
+                    store.getSimulationConstants.breedingMaxAge,
+                    store.getSimulationConstants.breedingMaxProgress
+                )
+                view.drawAnimal(entity.position,
+                    timestamp - entity.age.birthTimestamp,
+                    {
+                        gender: entity.gender,
+                        age: entity.age.current,
+                        isAlive: entity.isAlive,
+                        name: entity.name
+                    }
+                )
+                if (entity.currentActivity.activity === 'breeding') {
+                    view.drawBreeding({x: entity.position.x + 30, y: entity.position.y - 60})
+                }
+                if (entity.id === store.getActiveEntity?.id) {
+                    store.setActiveEntity(entity)
+                }
+            })
         }
     }, [context, canvasWidth, canvasHeight]);
 
@@ -120,6 +138,10 @@ const Scene = observer(({store, setAppPhase}: ISceneProps) => {
         ref={canvasRef}
         width={canvasWidth}
         height={canvasHeight}
+        onMouseDown={event => handleCanvasMousePress(event, mainCamera)}
+        onMouseUp={event => handleCanvasMouseRelease(event, mainCamera)}
+        onMouseMove={event => handleCanvasMouseMove(event, mainCamera)}
+        onWheel={event => handleCanvasMouseWheel(event, mainCamera)}
         onClick={event => handleCanvasClick(
             event,
             view,
