b633db810cb32f54ea1baccce27db940691856b4
Uladzimir Yahorau <evv2712@gmail.com>
2023-02-17 01:34:18 +0300

    lazy work - lazy results

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import classes from './App.module.scss'
 import Main from "./stages/Main/Main";
 import Simulation from "./stages/Simulation/Simulation";
-import Results from "./stages/Results/Results";
-import {useState} from "react";
+import {lazy, Suspense, useState} from "react";
 import {appPhase} from "./types";
 import Loader from "./components/Loader/Loader";
 import imagesSrc from "./img";
 import usePreloadedImages from "./hooks/useImagePreload";
 import ImageContext from './stores/ImageContext';
+
+const LazyResults = lazy(() => import("./stages/Results/Results"))
 
 const App = () => {
     const [phase, setPhase] = useState<appPhase>('NOT_STARTED')
@@ -16,7 +17,9 @@ const App = () => {
         <div className={classes.container}>
             {phase === 'NOT_STARTED' && <Main setAppPhase={setPhase}/>}
             {phase === 'STARTED' && <Simulation setAppPhase={setPhase}/>}
-            {phase === 'FINISHED' && <Results/>}
+            <Suspense fallback={<Loader/>}>
+                {phase === 'FINISHED' && <LazyResults/>}
+            </Suspense>
         </div>
     </ImageContext.Provider> : <Loader/>
 }
