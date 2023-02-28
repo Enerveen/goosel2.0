7e374706910973134964430cf7b12c56f8d0d086
Uladzimir Yahorau <evv2712@gmail.com>
2023-03-01 02:33:04 +0300

    Eggs spawn fixed

--- src/entities/Animal.ts
+++ src/entities/Animal.ts
@@ -448,7 +448,7 @@ class Animal extends Entity implements Movable {
                         return new Egg({
                             parents: {father, mother},
                             id: `A${getId()}`,
-                            position: {x: this.position.x - 20 + index * 10, y: this.position.y - 20 * index % 2},
+                            position: {x: this.position.x - 20 + index * 20, y: this.position.y - 20 * (index % 2)},
                             hatchTimestamp: timestamp + mother.stats.hatchingTime * simulationValuesMultipliers.hatchingTime
                         })
                     })
